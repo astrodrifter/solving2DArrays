9efae518f61fc861cf2ca730fa98bee676721d2f
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-21 13:28:05 +1000

    solved a modify array

---
+++ solving2DArrays/usingPointers/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* multiply each element by 2 */
+void modifyArray(int *numsPtr, int size)
+{
+    for (int i = 0; i < size; i++){
+        if(numsPtr[i] == -1)
+        {
+            numsPtr[i] = 9;
+        }
+    }
+}
+
+int main()
+{
+    ;
+    int nums[5] = {1, 2, -1, 3, -1};
+    modifyArray(nums, 5);
+    
+    //print
+    for(int i = 0; i < 5; i++)
+    {
+        printf("nums[%i] = %i\n",i, nums[i]);
+    }
+    return 0;
+}
